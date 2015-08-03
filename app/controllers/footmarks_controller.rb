86b0f8fe93a16f6f29ee38d72609fdcb2934b5c2
John van Arkelen <johnvanarkelen@gmail.com>
2015-08-03 20:39:11 +0200

    working on filter

--- app/controllers/footmarks_controller.rb
+++ app/controllers/footmarks_controller.rb
@@ -1,16 +1,10 @@
 class FootmarksController < ApplicationController
 
   def index
-    if params[:keyword].present?
-      @footmarks = Search.for(params[:keyword])
-    elsif params[:q].present?
-      @q = Footmark.search(params[:q])
-      @footmarks = @q.result.order('visit_date DESC').limit(10).page(params[:page])
-    else
-      @q = Footmark.search(params[:q])
-      @footmarks = Footmark.order('visit_date DESC').limit(10)
-    end
-
+    @q = Footmark.search(params[:q])
+    @footmarks = @q.result.order('visit_date DESC')
+    @footmarks = @footmarks.limit(10) if params[:q].blank?
+    @found = @footmarks.count unless @footmarks.blank?
     @clubs = Club.order(:name)
   end
 end
