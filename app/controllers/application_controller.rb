8f501d862070878ad8253d7b9b32c4f0dfa04227
John van Arkelen <johnvanarkelen@gmail.com>
2015-08-04 13:56:55 +0100

    added filter session storage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,28 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  def set_filter_state(filters)
+    params[:q] = {} unless params[:q]
+    filters.each do |filter|
+      cookies.delete(filter[1]) if params[:clear]
+
+      if params[:q]
+        unless params[:q][:s]
+          if params[:q][filter[0]]
+            cookies[filter[1]] = { value: params[:q][filter[0]], expires: 1.month.from_now }
+          else
+            params[:q][filter[0]] = cookies[filter[1]] unless cookies[filter[1]].blank?
+          end
+        end
+
+        if params[filter[0]]
+          cookies[filter[1]] = { value: params[filter[0]], expires: 1.month.from_now }
+        else
+          params[filter[0]] = cookies[filter[1]] unless cookies[filter[1]].blank?
+        end
+      end
+    end
+  end
+
 end
--- app/controllers/footmarks_controller.rb
+++ app/controllers/footmarks_controller.rb
@@ -1,8 +1,14 @@
 class FootmarksController < ApplicationController
 
   def index
+    filter = Array.new
+    filter << [:home_club_id_eq, :footmarks_home_club_id]
+    filter << [:away_club_id_eq, :footmarks_away_club_id]
+    filter << [:season_eq, :footmarks_season]
+    set_filter_state(filter)
+
     @q = Footmark.search(params[:q])
-    @footmarks = @q.result.includes(:home_club).includes(:away_club).order('visit_date DESC')
+    @footmarks = @q.result.includes(:home_club).includes(:away_club).order('nr DESC, visit_date DESC')
     @footmarks = @footmarks.limit(10) if params[:q].blank?
     @found = @footmarks.count unless @footmarks.blank?
     @clubs = Club.order(:name)
