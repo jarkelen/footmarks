f365f5084950e1343b12a54840be32b0c971c249
John van Arkelen <johnvanarkelen@gmail.com>
2015-08-06 08:14:15 +0200

    updated icons

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'dashboard#index'
 
+  resource :map, only: [:show]
   resources :footmarks
   resources :clubs, except: [:show]
   resources :leagues, except: [:show]
