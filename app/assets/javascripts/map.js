7aa922e3166b7a8cf9cb09c593f1e5bec5bd102a
John van Arkelen <johnvanarkelen@gmail.com>
2015-08-07 14:45:44 +0200

    repaired map

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,53 +1,44 @@
 $(document).ready(function() {
-  loadScript();
+  if ($("#map-canvas").length > 0){
+    var map;
+
+    var mapOptions = {
+      center: new google.maps.LatLng(54.8949509,-2.9116577),
+      zoom: 6,
+      mapTypeId: google.maps.MapTypeId.NORMAL,
+      panControl: true,
+      scaleControl: false,
+      streetViewControl: true,
+      overviewMapControl: true
+    };
+
+    // initializing map
+    map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions);
+
+    $.getJSON( 'map.json', function(data) {
+      $.each(data, function(index) {
+        if (data[index].longitude != ""){
+          var contentString = '<div class="marker_content">'+
+            '<p>'+data[index].visit_date+'</p>'+
+            '<h5 class="header">'+data[index].home_club.name+' v '+data[index].away_club.name+'</h5>'+
+            '<div class="link"><a href="/footmarks/'+data[index].id+'">details</a></div>'
+            '</div>';
+
+          var infowindow = new google.maps.InfoWindow({
+            content: contentString
+          });
+
+          var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(data[index].longitude,data[index].latitude),
+            map: map,
+            animation: google.maps.Animation.DROP
+          });
+          google.maps.event.addListener(marker, 'click', function() {
+            infowindow.open(map,marker);
+          });
+        }
+      });
+  	});
+
+  }
 });
-
-var map;
-
-function initialize() {
-  var mapOptions = {
-    center: new google.maps.LatLng(54.8949509,-2.9116577),
-    zoom: 6,
-    mapTypeId: google.maps.MapTypeId.NORMAL,
-    panControl: true,
-    scaleControl: false,
-    streetViewControl: true,
-    overviewMapControl: true
-  };
-  // initializing map
-  map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions);
-
-  $.getJSON( 'map.json', function(data) {
-    $.each(data, function(index) {
-      var contentString = '<div class="marker_content">'+
-        '<p>'+data[index].visit_date+'</p>'+
-        '<h5 class="header">'+data[index].home_club.name+' v '+data[index].away_club.name+'</h5>'+
-        '<div class="link"><a href="/footmarks/'+data[index].id+'">details</a></div>'
-        '</div>';
-
-      var infowindow = new google.maps.InfoWindow({
-          content: contentString
-      });
-
-      var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(data[index].longitude,data[index].latitude),
-        map: map,
-        animation: google.maps.Animation.DROP
-      });
-      google.maps.event.addListener(marker, 'click', function() {
-        infowindow.open(map,marker);
-      });
-
-    });
-
-
-	});
-
-}
-
-function loadScript() {
-  var script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&callback=initialize';
-  document.body.appendChild(script);
-}
--- app/models/footmark.rb
+++ app/models/footmark.rb
@@ -13,8 +13,4 @@ class Footmark < ActiveRecord::Base
     country = self.home_club.league.country
     "#{street} #{city} #{country}"
   end
-
-  def assign_nr
-    #vinks = Vink.where("user_id = :user_id and (vink_date < :vink_date)", user_id: vink.user_id, vink_date: vink.vink_date).count + 1
-  end
 end
