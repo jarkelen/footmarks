4e6f68442ab0b0daef011e8e9b86b5e9d090deca
John van Arkelen <johnvanarkelen@gmail.com>
2015-08-07 10:16:53 +0100

    added chart.js

--- app/assets/javascripts/charts.js
+++ app/assets/javascripts/charts.js
@@ -1,65 +1,43 @@
 if ($("#charts").length > 0){
-  google.load('visualization', '1', {'packages':['corechart','calendar']});
+  google.load('visualization', '1', {packages: ['corechart', 'bar']});
+  google.setOnLoadCallback(drawMultSeries);
 
-  google.setOnLoadCallback(drawChart);
-  function drawChart() {
-    // Create data for passed calendar
-    var data_array1 = JSON.parse($("#calendar_data_passed").html());
-    var data1 = new google.visualization.DataTable();
-    data1.addColumn({ type: 'date', id: 'Date' });
-    data1.addColumn({ type: 'number', id: 'Passed' });
-    for (var i = 0; i < data_array1.length; i++) {
-      data1.addRow([new Date(data_array1[i][0]), data_array1[i][1]]);
-    }
+  function drawMultSeries() {
+    var data = google.visualization.arrayToDataTable([
+      ['Testrun', 'Passed', 'Failed', 'Not executed', { role: 'annotation' } ],
+      ['10-05-15', 35, 8, 1, ''],
+      ['11-05-15', 34, 8, 2, ''],
+      ['12-05-15', 32, 10, 2, ''],
+      ['13-05-15', 35, 7, 2, ''],
+      ['14-05-15', 37, 6, 1, ''],
+      ['15-05-15', 36, 6, 2, ''],
+      ['16-05-15', 36, 6, 2, ''],
+      ['17-05-15', 38, 5, 1, ''],
+      ['18-05-15', 23, 16, 5, ''],
+      ['19-05-15', 33, 10, 1, ''],
+      ['20-05-15', 38, 5, 1, ''],
+      ['21-05-15', 40, 3, 1, '']
+    ]);
 
-    // Set data area chart
-    var data_array2 = JSON.parse($("#chart_data").html());
-    var data2 = google.visualization.arrayToDataTable(data_array2);
-
-    // Create data for failed calendar
-    var data_array3 = JSON.parse($("#calendar_data_failed").html());
-    var data3 = new google.visualization.DataTable();
-    data3.addColumn({ type: 'date', id: 'Date' });
-    data3.addColumn({ type: 'number', id: 'Failed' });
-    for (var i = 0; i < data_array3.length; i++) {
-      data3.addRow([new Date(data_array3[i][0]), data_array3[i][1]]);
-    }
-
-    // Set options for passed calendar
-    var options1 = {
-      title: "Passed test cases",
-      colorAxis: {colors: ['#E5FFE5', '#006600']}
+    /*
+    var jsonData = $.ajax({
+      url: "testruns.json",
+      dataType:"json",
+      async: false
+      }).responseText;
+    var data = new google.visualization.DataTable(jsonData);
+    */
+    var options = {
+      height: 400,
+      legend: 'none',
+      bar: { groupWidth: '50%' },
+      isStacked: true,
+        colors: ['#49bf67','#f34541', '#00acec']
     };
 
-    // Set options for area chart
-    var options2 = {
-      height: 300,
-      pointSize: 5,
-      chartArea:{left:50,top:30,width:'93%',height:'80%'},
-      isStacked: false,
-      colors: ['#49bf67','#f34541', '#00acec'],
-      hAxis: { textPosition: 'none' },
-      vAxis: { minValue: 0 },
-      legend: { position: 'bottom' }
-    };
+    var chart = new google.visualization.ColumnChart(
+      document.getElementById('testruns_chart'));
 
-    // Set options for failed calendar
-    var options3 = {
-      title: "Failed test cases",
-      colorAxis: {colors: ['#fcebe6', '#8a2208']}
-    };
-
-    // Draw area chart
-    var area_chart = new google.visualization.AreaChart(document.getElementById('testruns_chart'));
-    area_chart.draw(data2, options2);
-
-    // Draw calendar passed
-    var calendar_passed = new google.visualization.Calendar(document.getElementById('calendar_chart_passed'));
-    calendar_passed.draw(data1, options1);
-
-    // Draw calendar failed
-    var calendar_failed = new google.visualization.Calendar(document.getElementById('calendar_chart_failed'));
-    calendar_failed.draw(data3, options3);
+    chart.draw(data, options);
   }
-
 }
