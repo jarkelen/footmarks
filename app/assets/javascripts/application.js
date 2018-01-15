d004a0591e4d849cba51c6414916dae853727170
John van Arkelen <johnvanarkelen@gmail.com>
2018-01-15 16:25:29 +0100

    removed assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,4 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require flatty/jquery/jquery.min
-//= require flatty/jquery/jquery.mobile.custom.min
-//= require flatty/jquery/jquery-migrate.min
-//= require flatty/jquery/jquery-ui.min
-//= require flatty/bootstrap/bootstrap.min
-//= require flatty/plugins/plugins
-//= require flatty/theme
-//= require cocoon
-//= require charts
-//= require map
 //= require general
-//= require highcharts/highcharts
-//= require highcharts/highcharts-more
