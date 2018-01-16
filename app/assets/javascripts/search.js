e4d168f68855488ee20207ad8d270f72e4166389
John van Arkelen <johnvanarkelen@gmail.com>
2018-01-16 12:16:37 +0100

    added search

---
+++ app/assets/javascripts/search.js
@@ -0,0 +1,15 @@
+var ready;
+ready = function() {
+  $('#search').bind('keydown keypress keyup change', function() {
+    var search = this.value;
+    var $tr = $("#footmarks tr.footmark").hide();
+    $tr.filter(function() {
+      return $(this).text().toLowerCase().indexOf(search.toLowerCase()) >= 0;
+    }).show();
+
+    var rows = $('#footmarks tr:visible').length - 1;
+    $('.footmarks_count').text(rows + " footmarks gevonden");
+  });
+};
+$(document).ready(ready);
+$(document).on('page:load', ready);
