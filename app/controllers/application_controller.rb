41176f2e62e112604db7179eda6f43d5155acb7c
John van Arkelen <johnvanarkelen@gmail.com>
2018-01-16 08:37:05 +0100

    changed redirection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format.include? 'application/json' }
 
+  def after_sign_in_path_for(resource)
+    root_url
+  end
+  
   def after_sign_out_path_for(resource)
     new_user_session_path
   end
