313b56c879e46969c911dcddb3f78a1327f56ac3
John van Arkelen <johnvanarkelen@gmail.com>
2018-01-24 15:12:45 +0100

    updated form

--- app/models/footmark.rb
+++ app/models/footmark.rb
@@ -1,6 +1,7 @@
 class Footmark < ApplicationRecord
   belongs_to :home_club, class_name: "Club", foreign_key: :home_club_id
   belongs_to :away_club, class_name: "Club", foreign_key: :away_club_id
+  belongs_to :league
   has_many   :photos
 
   accepts_nested_attributes_for :photos, reject_if: :all_blank, allow_destroy: true
