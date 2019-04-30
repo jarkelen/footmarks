1815347397480d5f435f027aab0fb9db9772a152
John van Arkelen <john.van.arkelen@salves.nl>
2019-04-30 15:04:19 +0200

    working on layout

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -2,13 +2,13 @@ var ready;
 ready = function() {
   $('#search').bind('keydown keypress keyup change', function() {
     var search = this.value;
-    var $tr = $("#footmarks tr.footmark").hide();
+    var $tr = $("#search_items tr.item").hide();
     $tr.filter(function() {
       return $(this).text().toLowerCase().indexOf(search.toLowerCase()) >= 0;
     }).show();
 
-    var rows = $('#footmarks tr:visible').length - 1;
-    $('.footmarks_count').text(rows + " visits");
+    var rows = $('#search_items tr:visible').length - 1;
+    $('.search_count').text(rows + " visits");
   });
 };
 $(document).ready(ready);
